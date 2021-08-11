869a17f9f318cfb0bac0123ea898c0798eb7e262
catsital <catshital@gmail.com>
2021-08-11 17:45:39 +0800

    Add get option

--- bin/main.rb
+++ bin/main.rb
@@ -15,12 +15,16 @@ begin
 
     OptionParser.new do |parser|
       parser.on('-f', '--fetch URL',
-                'Fetch images from series or chapter URL') do |fetch_url|
+                'Fetch image(s) from a series or chapter link') do |fetch_url|
         print scrape(fetch_url).page_parse
       end
       parser.on('-i', '--info URL',
-                'Fetch page URL from series or chapter') do |content_url|
+                'Fetch page link(s) from a series or chapter page') do |content_url|
         print scrape(content_url).content_parse
+      end
+      parser.on('-g', '--get URL',
+                'Fetch image link(s) from an individual chapter page') do |image_url|
+        print scrape(image_url).image_parse(image_url)
       end
     end.parse!
   rescue Interrupt
